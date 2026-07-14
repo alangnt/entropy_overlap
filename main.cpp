2e5ea8bfa128afeab1435ee4b43ea9219aa5138c
Alan Geirnaert <geirnaert.alan@icloud.com>
2026-07-14 17:36:42 +0200

    feat: project init

---
+++ main.cpp
@@ -0,0 +1,194 @@
+#include <iostream>
+#include <numbers>
+#include <cmath>
+
+// Star:        R = 5
+// Planet:      r = 3
+// Separation:  d = 4
+
+// First, we confirm we're in the partial case
+// R - r = 2 and R + r = 8
+// 2 < 4 < 8 -> Good
+
+// Step 1. Locate the chords
+// 1. Star
+// x1 = ((d**2) + (R**2) - (r**2)) / (2 * d)
+// x1 = (16 + 25 - 9) / 8
+// x1 = 32 / 8
+// x1 = 4
+// 2. Planet
+// x2 = d - x1
+// x2 = 4 - 4
+// x2 = 0
+
+// Step 2. Find the height of the chord
+// h = sqrt((R**2) - (x1**2))
+// h = sqrt(25 - 16)
+// h = sqrt(9)
+// h = 3
+
+// This means the circles cross at (4,+3) and (4,-3)
+// Planet's center is (d,0) = (4,0) with r = 3
+// This means they both genuinely pass across (4,3)
+
+// Step 3. The two half-angles
+// 1. Star
+// θ_R = acos(x1 / R)
+// θ_R = acos(4/5)
+// θ_R = acos(0.8)
+// θ_R = 0.6435rad (36.87°)
+// 2. Planet
+// θ_r = acos(x2 / r)
+// θ_r = acos(0)
+// θ_r = pi/2
+// θ_r = 1.5708rad (90°)
+
+// Step 4. Add the two segments
+// R**2 * (θ − (sinθ * cosθ))
+// 1. Star
+// Star segment = 25 * (0.6435 - 0.6 * 0.8)
+// Start segment = 4.09
+// 2. Planet
+// Planet segment = 9 * (1.5708 - 1 * 0)
+// Planet segment = 14.14
+// Overlap = 18.23
+
+// Step 5. Cross-check
+// Term 1. R**2. * acos((d**2 + R**2 − r**2) / (2 * d * R)) = 16.09
+// Term 2. r**2 * acos((d**2 + r**2 − R**2) / (2 * d * r)) = 14.14
+// Term 3. 1/2 * sqrt((−d + R + r)(d + R − r)(d − R + r)(d + R +r)) = 12.00
+// 16.09 + 14.14 - 12.00 = 18.23
+
+// Step 6. Apply to real metrics
+// Blocked fraction = overlap / (pi * R**2)
+// Blocked fraction = 18.22 / 78.54
+// Blocked fraction = 0.232 = 23%
+
+const double R = 5.0;
+const double r = 3.0;
+const double d = 4.0;
+
+// Preliminary check
+bool checkIfPartiallyOverlaping() {
+  double distance1 = R - r;
+  double distance2 = R + r;
+
+  bool is_partially_overlaping = (distance1 < d) & (d < distance2);
+  std::cout << "Partially Overlaping: " << is_partially_overlaping << std::endl;
+  return is_partially_overlaping;
+}
+
+// Step 1
+double locateStarChord() {
+  // x1 = ((d**2) + (R**2) - (r**2)) / (2 * d)
+  double x1 = ((d*d) + (R*R) - (r*r)) / (2 * d);
+  return x1;
+}
+double locatePlanetChord(double x1) {
+  double x2 = d - x1;
+  return x2;
+}
+
+// Step 2
+double calculateStarChordHeight(double x1) {
+  // h = sqrt((R**2) - (x1**2))
+  double h = std::sqrt((R*R) - (x1*x1));
+  return h;
+}
+
+// Step 3
+double calculateStarHalfAngle(double x1) {
+  // θ_R = acos(x1 / R)
+  double theta_R = std::acos(x1 / R);
+  return theta_R;
+}
+double calculatePlanetHalfAngle(double x2) {
+  // θ_r = acos(x2 / r)
+  double theta_r = std::acos(x2 / r);
+  return theta_r;
+}
+
+// Step 4
+double calculateStarSegment(double theta_R) {
+  // R**2 * (θ − (sinθ * cosθ))
+  double star_segment = (R*R) * (theta_R - (std::sin(theta_R) * std::cos(theta_R)));
+  return star_segment;
+}
+double calculatePlanetSegment(double theta_r) {
+  // r**2 * (θ − (sinθ * cosθ))
+  double planet_segment = (r*r) * (theta_r - (std::sin(theta_r) * std::cos(theta_r)));
+  return planet_segment;
+}
+
+// Step 5
+bool crossCheck(double overlap) {
+  // Term 1. R**2 * acos((d**2 + R**2 − r**2) / (2 * d * R))
+  // Term 2. r**2 * acos((d**2 + r**2 − R**2) / (2 * d * r))
+  // Term 3. 1/2 * sqrt((−d + R + r) * (d + R − r) * (d − R + r) * (d + R + r))
+  double term1 = (R*R) * std::acos(((d*d) + (R*R) - (r*r)) / (2*d*R));
+  double term2 = (r*r) * std::acos(((d*d) + (r*r) - (R*R)) / (2*d*r));
+  double term3 = 0.5 * std::sqrt(((-d) + R + r) * (d + R - r) * (d - R + r) * (d + R + r));
+
+  double total_terms = term1 + term2 - term3;
+
+  bool checksOut = total_terms == overlap;
+  return checksOut;
+}
+
+// Step 6
+double calculateBlockedFraction(double overlap) {
+  // Blocked fraction = overlap / (pi * R**2)
+  double pi = std::numbers::pi;
+  double blocked_fraction_raw = overlap / (pi * (R*R));
+  double blocked_fraction = blocked_fraction_raw * 100;
+
+  return blocked_fraction;
+}
+
+int main() {
+  // Preliminary check
+  bool is_partially_overlaping = checkIfPartiallyOverlaping();
+
+  if (is_partially_overlaping == 1)
+  {
+    // Step 1
+    double x1 = locateStarChord();
+    double x2 = locatePlanetChord(x1);
+
+    // Step 2
+    double chord_height = calculateStarChordHeight(x1);
+
+    // Step 3
+    double theta_R = calculateStarHalfAngle(x1);
+    double theta_r = calculatePlanetHalfAngle(x2);
+
+    // Step 4
+    double star_segment = calculateStarSegment(theta_R);
+    double planet_segment = calculatePlanetSegment(theta_r);
+    double overlap = star_segment + planet_segment;
+
+    // Step 5
+    bool cross_check = crossCheck(overlap);
+
+    // Step 6
+    double blocked_fraction = calculateBlockedFraction(overlap);
+
+    std::cout << "Blocked Fraction: " << blocked_fraction << "%" << std::endl;
+    return 0;
+  }
+  else
+  {
+    // Check if totally overlaping
+    double distance = R - r;
+    bool is_totally_overlaping = d <= distance;
+
+    if (is_totally_overlaping) {
+      std::cout << "Blocked Fraction: 100%" << std::endl;
+      return 0;
+    }
+  }
+
+  // If not overlaping at all
+  std::cout << "Blocked Fraction: 0%" << std::endl;
+  return 0;
+}
---
+++ run.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [[ "$1" == "root" ]]; then
+  rm -rf build
+  mkdir build && cd build
+
+  cmake ..
+  cmake --build .
+else
+  cd build
+fi
+
+./entropy-collision
